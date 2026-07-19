a0ef0324e783e64db0d1bf1be4bd31d1bb3e6f68
wufeng <763476339@qq.com>
2026-07-19 11:00:08 +0800

    feat： 调整目录

--- core/detectors/detector_base.py
+++ core/detectors/detector_base.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 #Author: WuFeng <763467339@qq.com>
 #Date: 2026-07-17 10:45:35
-#LastEditTime: 2026-07-17 15:49:05
+#LastEditTime: 2026-07-19 10:59:18
 #LastEditors: WuFeng <763467339@qq.com>
 #Description: 接口定义
-#FilePath: /stamp-ai-service/core/detector_base.py
+#FilePath: /stamp-ai-service/core/detectors/detector_base.py
 #Copyright 版权声明
 #
 from abc import ABC, abstractmethod
--- core/detectors/detector_factory.py
+++ core/detectors/detector_factory.py
@@ -4,7 +4,7 @@
 #LastEditTime: 2026-07-17 15:49:51
 #LastEditors: WuFeng <763467339@qq.com>
 #Description: 实例化逻辑
-#FilePath: /stamp-ai-service/core/detector_factory.py
+#FilePath: /stamp-ai-service/core/detectors/detector_factory.py
 #Copyright 版权声明
 #
 from core.detectors.detector_base import BaseDetector
